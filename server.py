1ff725f5cb52a44e73433e2a144fce4f1e8f8c88
Danny Nguyen <nguyenduongpa@gmail.com>
2023-04-18 19:21:40 -0700

    second commit, added more files

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+from flask_app.controllers import users
+from flask_app import app
+from flask import Flask, render_template
 
 if __name__ == "__main__":
     app.run(debug=True)
