055117825c61bf1217e89f6ec163b0c5913a6361
Anthony Kim <anthonykim92@gmail.com>
2023-04-20 12:20:32 -0700

    added controllers/models

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,9 +1,59 @@
 from flask_app import app
 from flask import render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt
+from flask_app.models.user import User
+from flask_app.models.player import Player
 bcrypt = Bcrypt(app)
 
 
 @app.route('/')
 def index_page():
     return render_template('index.html')
+
+@app.route('/register', methods=["POST"])
+def register():
+    print(request.form)
+    if not User.validate_user(request.form):
+        return redirect('/')
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    print(pw_hash)
+    data = {
+        "id" : request.form["id"],
+        "first_name": request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"],
+        "password" : pw_hash
+        }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/dash')
+
+@app.route('/login', methods = ['POST'])
+def login():
+
+    print(request.form)
+
+    data = { "email" : request.form["email"] }
+    user_in_db = User.get_by_email(data)
+    if not user_in_db:
+        flash("Invalid Email/Password")
+        return redirect("/")
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Email/Password")
+        return redirect('/')
+    session['user_id'] = user_in_db.id
+    return redirect("/dash")
+
+@app.route("/users/account/<int:user_id>")
+def edit(user_id):
+    data = {
+        'id': user_id
+    }
+    user = User.get_by_id(data)
+    players = Player.get_all_with_maker()
+    return render_template("edit.html", user=user, players = players)
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -1,7 +1,95 @@
-import re
 from flask_app.config.mysqlconnection import connectToMySQL
-from flask_bcrypt import Bcrypt
 from flask_app import app
-from flask import flash
+from flask_app.models import player
+from flask_bcrypt import Bcrypt        
 bcrypt = Bcrypt(app)
+
+import re
+from flask import flash
+
+
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.firstname = data['firstname']
+        self.lastname = data['lastname']
+        self.password = data['password']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.players_made = []
+
+
+    @staticmethod
+    def validate_user( user ):
+        is_valid = True
+
+        if len(user['firstname']) < 3:
+            flash("First name must be at least 3 characters.")
+            print('firstname flash')
+            is_valid = False
+        if len(user['lastname']) < 3:
+            flash("Last name must be at least 3 characters.")
+            print('lastname flash')
+            is_valid = False
+
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Please use the correct email format")
+            print('email flash')
+            is_valid = False
+        if len(user['password']) < 8:
+            flash("Password must be longer.")
+            print('password flash')
+            is_valid = False
+
+        if user['password'] != user['confirm_pass']:
+            flash("Passwords do not match.")
+            print('confirm password flash')
+            is_valid = False
+
+        data = {
+            'email': user['email']
+        }
+
+        user_in_db = User.get_by_email(data)
+        if user_in_db:
+            flash('Email already in database.')
+            is_valid = False
+        return is_valid
+
+
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO users ( firstname, lastname, email, password ) VALUES ( %(firstname)s , %(lastname)s, %(email)s, %(password)s );"
+        results = connectToMySQL('users_and_players').query_db( query, data )
+
+        print(results)
+        return results
+
+    @classmethod
+    def get_all_users(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('users_and_players').query_db(query)
+        users = []
+        for one_user in results:
+            users.append( cls(one_user) )
+        print(users)
+        return users
+
+    @classmethod
+    def get_by_id(cls, data):
+        query = "SELECT * FROM users WHERE id =%(id)s;"
+
+        results = connectToMySQL('users_and_players').query_db(query, data )
+        print(results)
+        return cls(results[0])
+
+    @classmethod
+    def get_by_email(cls, data):
+        query = "SELECT * FROM users WHERE email =%(email)s;"
+        results = connectToMySQL('users_and_players').query_db(query, data )
+        if results == ():
+            return False
+        return cls(results[0])
