c800650bff1450761c431d12b79d644495defa96
Johnathon Bowers <johnathonbowers@gmail.com>
2023-04-23 23:08:44 -0400

    finished edit and delete functionality, improved validations and routing

--- flask_app/controllers/players.py
+++ flask_app/controllers/players.py
@@ -33,10 +33,36 @@ def process_player():
         return redirect('/logout')
     
     if not player.Player.validate_player(request.form):
+        session['first_name'] = request.form.get('first_name')
+        session['last_name'] = request.form.get('last_name')
+        session['team'] = request.form.get('team')
+        session['position'] = request.form.get('position')
+        session['points_pg'] = request.form.get('points_pg')
+        session['rebounds_pg'] = request.form.get('rebounds_pg')
+        session['assists_pg'] = request.form.get('assists_pg')
+        session['steals_pg'] = request.form.get('steals_pg')
+        session['blocks_pg'] = request.form.get('blocks_pg')
         return redirect('/players/create')
-    
-    player.Player.save_player(request.form)
-    return redirect('/dashboard')
+    player_id = player.Player.save_player(request.form)
+    if session.get('first_name'):
+        session.pop('first_name')
+    if session.get('last_name'):
+        session.pop('last_name')
+    if session.get('team'):
+        session.pop('team')
+    if session.get('position'):
+        session.pop('position')
+    if session.get('points_pg'):
+        session.pop('points_pg')
+    if session.get('rebounds_pg'):
+        session.pop('rebounds_pg')
+    if session.get('assists_pg'):
+        session.pop('assists_pg')
+    if session.get('steals_pg'):
+        session.pop('steals_pg')
+    if session.get('blocks_pg'):
+        session.pop('blocks_pg')
+    return redirect(f'/players/{player_id}')
 
 @app.route('/players/<int:id>')
 def display_player(id):
@@ -65,9 +91,22 @@ def display_edit_player_form(id):
     
 @app.route('/players/edit-submit', methods=['POST'])
 def edit_player():
+    player_id = request.form.get('player_id')
     if not player.Player.validate_player(request.form):
-        player_id = request.form.get('player_id')
         return redirect (f'/players/{player_id}/edit')
     player.Player.edit_player(request.form)
-    return redirect ('/dashboard')
+    return redirect (f'/players/{player_id}')
 
+@app.route('/players/<int:id>/delete')
+def delete_player(id):
+    if not session.get('user_id'):
+        return redirect('/login')
+    if session.get('user_id'):
+        player_data = {
+            'player_id': id
+        }
+        returned_player = player.Player.get_one_player_with_creator(player_data)
+        if session.get('user_id') != returned_player.creator.id:
+            return redirect('/dashboard')
+        player.Player.delete_player(player_data)
+        return redirect('/dashboard')
--- flask_app/models/player.py
+++ flask_app/models/player.py
@@ -22,31 +22,46 @@ class Player:
     @staticmethod
     def validate_player(player):
         is_valid = True
-        if len(player['first_name']) < 2:
+        if len(player.get('first_name')) < 2:
             flash('Player first name must be at least two characters long', 'player')
             is_valid = False
-        if len(player['last_name']) < 2:
+        if len(player.get('last_name')) < 2:
             flash('Player last name must be at least two characters long', 'player')
             is_valid = False
-        if not player['team']:
+        if not player.get('team'):
             flash('Please select a team', 'player')
             is_valid = False
-        if not player['position']:
+        if not player.get('position'):
             flash('Please select a position', 'player')
             is_valid = False
-        if float(player['points_pg']) < 0:
+        if not player.get('points_pg'):
+            flash('Please enter points per game', 'player')
+            is_valid = False
+        if player.get('points_pg') and float(player.get('points_pg')) < 0:
             flash('Points per game must be zero or higher', 'player')
             is_valid = False
-        if float(player['rebounds_pg']) < 0:
+        if not player.get('rebounds_pg'):
+            flash('Please enter rebounds per game', 'player')
+            is_valid = False
+        if player.get('rebounds_pg') and float(player.get('rebounds_pg')) < 0:
             flash('Rebounds per game must be zero or higher', 'player')
             is_valid = False
-        if float(player['assists_pg']) < 0:
+        if not player.get('assists_pg'):
+            flash('Please enter assists per game', 'player')
+            is_valid = False
+        if player.get('assists_pg') and float(player.get('assists_pg')) < 0:
             flash('Assists per game must be zero or higher', 'player')
             is_valid = False
-        if float(player['steals_pg']) < 0:
+        if not player.get('steals_pg'):
+            flash('Please enter steals per game', 'player')
+            is_valid = False
+        if player.get('steals_pg') and float(player.get('steals_pg')) < 0:
             flash('Steals per game must be zero or higher', 'player')
             is_valid = False
-        if float(player['blocks_pg']) < 0:
+        if not player.get('blocks_pg'):
+            flash('Please enter blocks per game', 'player')
+            is_valid = False
+        if player.get('blocks_pg') and float(player.get('blocks_pg')) < 0:
             flash('Blocks per game must be zero or higher', 'player')
             is_valid = False
         return is_valid
@@ -113,108 +128,8 @@ class Player:
     def edit_player(cls, data):
         query = 'UPDATE players SET first_name = %(first_name)s, last_name = %(last_name)s, team = %(team)s, position = %(position)s, points_pg = %(points_pg)s, rebounds_pg = %(rebounds_pg)s, assists_pg = %(assists_pg)s, steals_pg = %(steals_pg)s, blocks_pg = %(blocks_pg)s WHERE id = %(player_id)s;'
         return connectToMySQL('stat_sheet_schema').query_db(query, data)
-
-    # @classmethod
-    # def create_a_player(cls,data):
-    #     query = "INSERT INTO players (playername,playerteam,playerposition,playerppg,playerrpg,playerapg,playerspg,playerbpg, users_id) VALUES (%(playername)s , %(playerteam)s, %(playerposition)s , %(playerppg)s, %(playerrpg)s , %(playerapg)s, %(playerspg)s , %(playerbpg)s, %(users_id)s );"
-    #     return connectToMySQL('users_and_players').query_db( query, data )
-
-
-    # @staticmethod
-    # def validate_player(player):
-    #     is_valid = True
-    #     if len(player['playername']) < 2:
-    #         flash(" Player Name must be at least 2 characters.")
-    #         is_valid = False
-    #     if len(player['playerteam']) < 10:
-    #         flash("Player Team must be at least 10 characters.")
-    #         is_valid = False
-    #     if len(player['playerposition']) < 2:
-    #         flash(" Player Position must be at least 2 characters.")
-    #         is_valid = False
-    #     if len(player['playerppg']) < 0:
-    #         flash("PPG must be at least 0.")
-    #         is_valid = False
-    #     if len(player['playerrpg']) < 0:
-    #         flash("RPG must be at least 0.")
-    #         is_valid = False
-    #     if len(player['playerapg']) < 0:
-    #         flash("APG must be at least 0.")
-    #         is_valid = False
-    #     if len(player['playerspg']) < 0:
-    #         flash("SPG must be at least 0.")
-    #         is_valid = False
-    #     if len(player['playerbpg']) < 0:
-    #         flash("BPG must be at least 0.")
-    #         is_valid = False
-    #     return is_valid
-    # @classmethod
-    # def get_all_with_maker(cls):
-    #     query = "SELECT * from players JOIN users ON users.id = players.users_id;"
-    #     results = connectToMySQL('users_and_players').query_db( query)
-    #     all_players = []
-    #     for row in results:
-    #         all_player = cls(row)
-    #         user_data = {
-    #             "id" :row['users.id'],
-    #             "firstname" :row['firstname'],
-    #             "lastname" :row['lastname'],
-    #             "password" :None,
-    #             "email" :row['email'],
-    #             "updated_at" :None,
-    #             "created_at" :None,
-                
-    #         }
-    #         user_obj = user.User(user_data)
-    #         all_player.maker = user_obj
-    #         all_players.append(all_player)
-    #     return all_players
-        
-    # @classmethod
-    # def get_all_players(cls):
-    #     query = "SELECT * FROM players;"
-    #     results = connectToMySQL('users_and_players').query_db(query)
-    #     players = []
-    #     for one_player in results:
-    #         players.append( cls(one_player) )
-    #     print(players)
-    #     return players
     
-    # @classmethod
-    # def get_player_by_id(cls, data):
-    #     query = "SELECT * FROM players JOIN users ON players.user_id = users.id WHERE players.id = %(id)s;"
-    #     results = connectToMySQL('users_and_players').query_db(query,data)
-    #     one_player= cls(results[0])
-    #     one_player_maker_info = {
-    #         "id": results[0]['users.id'],
-    #         "created_at": results[0],
-    #         "firstname": results[0]['firstname'],
-    #         "lastname" : results[0]['lastname'],
-    #         "email" : results[0]['email'],
-    #         "updated_at":results[0]['users.updated_at'],
-    #         "password": None,
-    #     }
-    #     author = user.User(one_player_maker_info)
-    #     one_player.maker = author
-
-    #     return one_player
-    
-    # @classmethod
-    # def update(cls, player_dict):
-
-    #     player = cls.get_by_id(player_dict)
-
-    #     query = """UPDATE players
-    #                 SET (playername,playerteam,playerposition,playerppg,playerrpg,playerapg,playerspg,playerbpg, user_id)
-    #                 WHERE id = %(id)s;"""
-    #     return connectToMySQL('users_and_players').query_db(query,player_dict)
-
-
-    # @classmethod
-    # def delete(cls, player_id):
-
-    #     data = {"id": player_id}
-    #     query = "DELETE from players WHERE id = %(id)s;"
-    #     connectToMySQL('users_and_players').query_db(query,data)
-
-    #     return player_id
+    @classmethod
+    def delete_player(cls, data):
+        query = 'DELETE FROM players WHERE id = %(player_id)s'
+        return connectToMySQL('stat_sheet_schema').query_db(query, data)
